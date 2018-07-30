39f53937df3a0242d94937f4d8e9357e092e1ebe
Ricardo Pereira <rpdesignerfly@gmail.com>
2018-07-30 18:26:31 -0300

    Adicionados cabeçalhos de copyright e declaração de strict_types

--- src/Libs/RevertObfuscation.php
+++ src/Libs/RevertObfuscation.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @see       https://github.com/rpdesignerfly/artisan-obfuscator
+ * @copyright Copyright (c) 2018 Ricardo Pereira Dias (https://rpdesignerfly.github.io)
+ * @license   https://github.com/rpdesignerfly/artisan-obfuscator/blob/master/license.md
+ */
+
+declare(strict_types=1);
 
 // fake deflates
 function cfForgetShow($data, $revert = FALSE) {
@@ -24,24 +31,24 @@ function sagaPlus($data, $revert = FALSE) {
 }
 
 function baseOne($data, $revert = FALSE) {
-    
+
     if ($revert == FALSE) {
         $encoded = base64_encode($data);
-        
+
         // Separa em dois pedaços
         $partOne = mb_substr($encoded, 0, 10, "utf-8");
         $partTwo = mb_substr($encoded, 10, NULL, "utf-8");
-        
+
         // Insere 'Sg' para invalidar o base64
         return $partOne . 'Sg' . $partTwo;
-        
+
     }
     else {
-        
+
         // Separa em dois pedaços
         $partOne = mb_substr($data, 0, 10, "utf-8");
         $partTwo = mb_substr($data, 12, NULL, "utf-8");
-        
+
         // Remove 'Sg' para validar o base64
         return base64_decode($partOne . $partTwo);
     }
@@ -50,21 +57,21 @@ function baseOne($data, $revert = FALSE) {
 function baseTwo($data, $revert = FALSE) {
     if ($revert == FALSE) {
         $encoded = base64_encode($data);
-        
+
         // Separa em dois pedaços
         $partOne = mb_substr($encoded, 0, 5, "utf-8");
         $partTwo = mb_substr($encoded, 5, NULL, "utf-8");
-        
+
         // Insere 'Sg' para invalidar o base64
         return $partOne . 'Sg' . $partTwo;
-        
+
     }
     else {
-        
+
         // Separa em dois pedaços
         $partOne = mb_substr($data, 0, 5, "utf-8");
         $partTwo = mb_substr($data, 7, NULL, "utf-8");
-        
+
         // Remove 'Sg' para validar o base64
         return base64_decode($partOne . $partTwo);
     }
@@ -73,21 +80,21 @@ function baseTwo($data, $revert = FALSE) {
 function baseThree($data, $revert = FALSE) {
     if ($revert == FALSE) {
         $encoded = base64_encode($data);
-        
+
         // Separa em dois pedaços
         $partOne = mb_substr($encoded, 0, 15, "utf-8");
         $partTwo = mb_substr($encoded, 15, NULL, "utf-8");
-        
+
         // Insere 'Sg' para invalidar o base64
         return $partOne . 'Sg' . $partTwo;
-        
+
     }
     else {
-        
+
         // Separa em dois pedaços
         $partOne = mb_substr($data, 0, 15, "utf-8");
         $partTwo = mb_substr($data, 17, NULL, "utf-8");
-        
+
         // Remove 'Sg' para validar o base64
         return base64_decode($partOne . $partTwo);
     }
@@ -99,4 +106,3 @@ function unsetLogger(){ return TRUE; }
 function loopNested(){ return TRUE; }
 function vorticeData(){ return TRUE; }
 function cipherBinary(){ return TRUE; }
-
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @see       https://github.com/rpdesignerfly/artisan-obfuscator
+ * @copyright Copyright (c) 2018 Ricardo Pereira Dias (https://rpdesignerfly.github.io)
+ * @license   https://github.com/rpdesignerfly/artisan-obfuscator/blob/master/license.md
+ */
+
+declare(strict_types=1);
 
 namespace Obfuscator;
 
